5504ce393be378f8d8f63b21e11d2bf2fdd3eea2
fouadbuilds <223906045+fouadbuilds@users.noreply.github.com>
2026-02-25 22:47:44 -0400

    style/ added ascii introduction

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Command } from "commander";
+import picocolors from "picocolors";
 import { getCommand } from "./commands/get.js";
 import { saveCommand } from "./commands/save.js";
 import { deleteCommand } from "./commands/delete.js";
@@ -6,17 +7,39 @@ import { listCommand } from "./commands/list.js";
 
 const program = new Command();
 
+const logo = picocolors.cyan(String.raw`
+                     /$$           /$$   /$$    
+                    |__/          |__/  | $$    
+  /$$$$$$$ /$$$$$$$  /$$  /$$$$$$  /$$ /$$$$$$  
+ /$$_____/| $$__  $$| $$ /$$__  $$| $$|_  $$_/  
+|  $$$$$$ | $$  \ $$| $$| $$  \ $$| $$  | $$    
+ \____  $$| $$  | $$| $$| $$  | $$| $$  | $$ /$$
+ /$$$$$$$/| $$  | $$| $$| $$$$$$$/| $$  |  $$$$/
+|_______/ |__/  |__/|__/| $$____/ |__/   \___/  
+                        | $$                    
+                        | $$                    
+                        |__/                    
+`);
+
+
+const tagline = `${picocolors.yellow('Stop re-typing; start snipping.')}\n`;
+
 program
   .name('snipit')
   .description('A local snippet manager')
-  .version('1.0.0')
+  .version('1.1.1')
+  .addHelpText('before', logo + tagline);
   
 
-  // Commands
-  program.addCommand(saveCommand);
-  program.addCommand(getCommand)
-  program.addCommand(deleteCommand)
-  program.addCommand(listCommand)
-  
+// Commands
+program.addCommand(saveCommand);
+program.addCommand(getCommand);
+program.addCommand(deleteCommand);
+program.addCommand(listCommand);
 
 program.parse(process.argv);
+
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
